d6aa4193ca1dbeca1e573146a8f8394aee68ae32
Dikmit02 <dikshamittal21297@gmail.com>
2021-07-14 18:21:14 +0530

    layout files

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,40 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+@section('title')
+Single Todo: {{ $todo->name }}
+@endsection
 
-    <title>Todo Item</title>
-</head>
+@section('content')
+<h1 class="text-center my-5">
+  {{ $todo->name }}
+</h1>
 
-<body>
-    <div class="conatiner">
+<div class="row justify-content-center">
+  <div class="col-md-6">
+    <div class="card card-default">
+      <div class="card-header">
+        Details
+      </div>
 
-        <h1 class="text-center my-5">
-            {{$todo->name}}
-        </h1>
-
-
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card card-default">
-                    <div class="card-header">
-                        Details
-                    </div>
-
-                    <div class="card-body">
-                        {{ $todo->description }}
-                    </div>
-                </div>
-            </div>
-        </div>
-
+      <div class="card-body">
+        {{ $todo->description }}
+      </div>
     </div>
-
-
-</body>
-
-</html>
+  </div>
+</div>
+@endsection
