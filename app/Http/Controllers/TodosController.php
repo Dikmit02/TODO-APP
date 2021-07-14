40333edf962cb236533841a4cf1d26964c7bc35f
Dikmit02 <dikshamittal21297@gmail.com>
2021-07-14 22:35:25 +0530

    update todo

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -46,7 +46,7 @@ class TodosController extends Controller
             'name' => 'required|min:6|max:12',
             'description' => 'required'
           ]);
-          
+
         // dd(request()->all());
         $data = request()->all();
 
@@ -58,5 +58,31 @@ class TodosController extends Controller
         $todo->save();
   
         return redirect('/todos');
+    }
+
+    public function edit($todoId)
+    {
+      $todo = Todo::find($todoId);
+
+      return view('todos.edit')->with('todo', $todo);
+    }
+
+    public function update($todoId)
+    {
+      $this->validate(request(), [
+        'name' => 'required|min:6|max:12',
+        'description' => 'required'
+      ]);
+
+      $data = request()->all();
+
+      $todo = Todo::find($todoId);
+
+      $todo->name = $data['name'];
+      $todo->description = $data['description'];
+
+      $todo->save();
+
+      return redirect('/todos');
     }
 }
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -6,20 +6,21 @@ Single Todo: {{ $todo->name }}
 
 @section('content')
 <h1 class="text-center my-5">
-  {{ $todo->name }}
+    {{ $todo->name }}
 </h1>
 
 <div class="row justify-content-center">
-  <div class="col-md-6">
-    <div class="card card-default">
-      <div class="card-header">
-        Details
-      </div>
+    <div class="col-md-6">
+        <div class="card card-default">
+            <div class="card-header">
+                Details
+            </div>
 
-      <div class="card-body">
-        {{ $todo->description }}
-      </div>
+            <div class="card-body">
+                {{ $todo->description }}
+            </div>
+            <a href="/todos/{{ $todo->id }}/edit" class="btn btn-info btn-sm my-2">Edit</a>
+        </div>
     </div>
-  </div>
 </div>
 @endsection
